138ab0b62d050840259225e8b9dd6d3e63558ef1
Shintaro Fujiwara <sfujiwara1993@gmail.com>
2025-03-12 18:57:49 +0900

    add floyd

---
+++ linked-list-cycle/floyd.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasCycle(self, head):
+        if head is None:
+            return False
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            if slow == fast:
+                return True
+            slow = slow.next
+            fast = fast.next.next
+        return False
